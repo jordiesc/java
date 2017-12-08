b786eeeda9d094dd9ddd7f2960b177c0d82c828a
jordi <jordi.escofet@seat.es>
2017-12-08 10:08:56 +0100

    adding H2 JPA test standalone without container

---
+++ hibernate/hibernateh2alone/hibernate/src/main/java/com/jordi/model/Proyecto.java
@@ -0,0 +1,94 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.jordi.model;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author SESCO31
+ */
+@Entity
+public class Proyecto implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    
+    private String codigo;
+    private String descripcion;
+
+    public Proyecto(String codigo, String descripcion, boolean activo) {
+        this.codigo = codigo;
+        this.descripcion = descripcion;
+        this.activo = activo;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public boolean isActivo() {
+        return activo;
+    }
+
+    public void setActivo(boolean activo) {
+        this.activo = activo;
+    }
+    private boolean activo;
+            
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Proyecto)) {
+            return false;
+        }
+        Proyecto other = (Proyecto) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "com.jordi.model.Proyecto[ id=" + id + " ]";
+    }
+    
+}
---
+++ hibernate/hibernateh2alone/hibernate/src/test/java/com/jordi/hibernate/JPAH2JUnitTest.java
@@ -0,0 +1,70 @@
+package com.jordi.hibernate;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import com.jordi.model.Proyecto;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+import javax.persistence.Entity;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+/**
+ *
+ * @author SESCO31
+ */
+public class JPAH2JUnitTest {
+
+    public JPAH2JUnitTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() {
+    }
+
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+  
+    public void setUp() {
+
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void loadDatabase() {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("h2");
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+            em.persist(new Proyecto("pia", "la pia no mola", true));
+            em.persist(new Proyecto("was", "el was mola mas", true));
+            em.persist(new Proyecto("mobile", "vaya kk de projecto", true));
+
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+            emf.close();
+        }
+    }
+
+}
