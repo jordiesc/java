0339c14a2912ebac1f254ac61f209c1a5cdd9fff
joresc <jordi.escofet@gmail.com>
2019-11-17 09:04:20 +0100

    srpingbasic

---
+++ springbasic/src/main/java/org/jordi/springbasic/App.java
@@ -0,0 +1,13 @@
+package org.jordi.springbasic;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
